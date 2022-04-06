ee8ff3058306ec5ff8eb849385fd0a6d55871f5d
AndresOberhelman <andres.w.oberhelman@gmail.com>
2022-04-06 13:44:49 -0500

    backend work

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,25 @@
 const express = require('express');
-const cors=require("cors");
+const cors = require('cors');
 const mongoose = require('mongoose');
-import listings from "./api/listing.route.js"
+
 require('dotenv').config();
-const app=express();
-const port=process.env.PORT || 5000;
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/login",login)
-app.use("*",(req,res)=>res.status(404).json({error:"not found"}))
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true}
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
-export default app
-
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex: true}
-// );
-// const connection = mongoose.connection;
-// connection.once('open', ()=>{
-//   console.log("MongoDB database connection established successfully");    
-// });
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
 
 
-// app.listen(port, () => {
-//     console.log('Server is running on port: $');
-// });
